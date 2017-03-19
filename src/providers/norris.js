77b2fdd979b78919de139feaacfa0d92becfd148
Patrick Metzdorf <patrick.metzdorf@gmail.com>
2017-03-19 21:25:26 +0000

    small reformatting

--- src/providers/norris.js
+++ src/providers/norris.js
@@ -3,16 +3,17 @@ import { decode } from 'he'
 
 async function getWise (fullName) {
   const [ firstName, lastName ] = fullName.split(' ')
-  const url = `http://api.icndb.com/jokes/random?firstName=${firstName.trim()}&lastName=${lastName.trim()}`
 
+  const url = `http://api.icndb.com/jokes/random?firstName=${firstName.trim()}&lastName=${lastName.trim()}`
   let response
 
   try {
     response = await axios.get(url)
-    if (response.data.type !== 'success') throw new Error('Error recalling Chuck Norris wisdom')
+    if (!response.data || response.data.type !== 'success') {
+      throw new Error('Error recalling Chuck Norris wisdom')
+    }
   } catch (e) {
-    console.error(e)
-    return
+    return console.error(e)
   }
 
   return decode(response.data.value.joke)
